b3d50290ecc9ef828206c66148761687814babe3
dzering <71943051+dzering@users.noreply.github.com>
2022-02-06 23:46:34 +0400

    Add PlayerModel
    Add GameState, CarModel, PlayerModel

---
+++ Assets/_Root/Scripts/PlayerModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class PlayerModel
+{
+    public GameState GameState { get; set; }
+    public CarModel Car { get; set; }
+
+    public PlayerModel()
+    {
+        GameState = GameState.None;
+        Car = new CarModel(5);
+    }
+
+}
