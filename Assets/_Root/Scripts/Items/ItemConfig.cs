334e5c00127f343b286520535c2f1daa523ff270
dzering <71943051+dzering@users.noreply.github.com>
2022-02-15 00:07:00 +0400

    Update

---
+++ Assets/_Root/Scripts/Items/ItemConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "MyConfigs/Item", order = 0)]
+public class ItemConfig : ScriptableObject
+{
+    [SerializeField]
+    private int id;
+    [SerializeField]
+    private string title;
+
+    public int Id { get => id; }
+    public string Title { get => title; }
+}
+
+internal enum UpgradeType
+{
+    None,
+    Speed,
+    Health
+}
