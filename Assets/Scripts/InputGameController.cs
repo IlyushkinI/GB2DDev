9a39ef3c3142ac64d66958c57cb9d25a8f325758
Vasco-PC <kravchuk.vasilii@yandex.ru>
2022-04-19 15:50:15 +0300

    Button controller
    Button controller for car moving was added.

--- Assets/Scripts/InputGameController.cs
+++ Assets/Scripts/InputGameController.cs
@@ -9,14 +9,15 @@ public class InputGameController : BaseController
         _view.Init(leftMove, rightMove, car.Speed);
     }
 
-    private readonly ResourcePath _viewPath = new ResourcePath {PathResource = "Prefabs/StickControl"};
+    //private readonly ResourcePath _viewPath = new ResourcePath { PathResource = "Prefabs/StickControl" };
+    private readonly ResourcePath _viewPath = new ResourcePath { PathResource = "Prefabs/ButtonControl" };
     private BaseInputView _view;
 
     private BaseInputView LoadView()
     {
         var objView = Object.Instantiate(ResourceLoader.LoadPrefab(_viewPath));
         AddGameObjects(objView);
-        
+
         return objView.GetComponent<BaseInputView>();
     }
 }
